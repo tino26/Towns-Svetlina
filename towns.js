9145afbd3f65ac80601765492cdfc1c9542898c4
uxperience-avgeorgiev <av@uxperience.eu>
2024-01-12 13:40:18 +0200

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -17,3 +17,29 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+
+function addTown() {
+	let townName = document.getElementById("townNameForAdd").value;
+	if (townName != "") {	
+		document.getElementById("townNameForAdd").value = "";
+		let option = document.createElement("option");
+		option.textContent = townName;
+		document.getElementById("towns").appendChild(option);
+		showMessage(townName + " added.");
+	}
+}
+
+function showMessage(msg) {
+	let resultElement = document.getElementById("result");
+	resultElement.textContent = msg;
+	resultElement.style.display = "block";
+
+	setTimeout(function () {
+		resultElement.style.display = "none";
+	}, 3000);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("btnAdd").addEventListener("click", addTown);
+});
