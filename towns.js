2246f787d9f368412f94c32dd3a024bf9a9be668
Blagovest-Hristov <bobo4e.h@gmail.com>
2024-01-12 13:35:49 +0200

    DOBRE SVURSHENA RABOTA :)
    ehe

--- towns.js
+++ towns.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+///get shuffle btn and add click event
+$(document).ready(function() {
+	$('#btnShuffle').click(shuffleTowns)
+});
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
@@ -16,4 +21,14 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function shuffleTowns() {
+	let towns = $('#towns option');
+	for (let i = 0; i < towns.length; i++) {
+		let j = Math.floor(Math.random() * towns.length);
+		let temp = towns[i].textContent;
+		towns[i].textContent = towns[j].textContent;
+		towns[j].textContent = temp;
+	}
 }
